4a31cad3805f4fb37c11cd2616c966d214b10a68
sim <sim@DESKTOP-GMUQI4M>
2021-03-08 23:29:41 +0900

    deleted hello.java and added menumanager.java

---
+++ src/MenuManger.java
@@ -0,0 +1,12 @@
+
+public class MenuManger {
+
+	public static void main(String[] args) {
+		System.out.println("1. add student");
+		System.out.println("2. delete student");
+		System.out.println("3. edit student");
+		System.out.println("4. view student");
+		System.out.println("5. exit");
+	}
+
+}
