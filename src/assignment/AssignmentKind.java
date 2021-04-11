89e364cc05c58da8493033932def1ab276e53096
sim <sim@DESKTOP-GMUQI4M>
2021-04-11 13:01:47 +0900

    new kind of assignment

---
+++ src/assignment/AssignmentKind.java
@@ -0,0 +1,9 @@
+package assignment;
+
+public enum AssignmentKind {
+	University, //학교수업과제(ex. 객프밍 과제#1)
+	Personal,   //학교수업과제가 아닌 개인적인 공부의 과제(ex.토익단어 10개 외우기)
+	Quick     //간단한 정보만을 입력
+	
+	
+}
---
+++ src/assignment/PersonalAssignment.java
@@ -0,0 +1,40 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class PersonalAssignment extends Assignment {
+
+	public void getUserInput(Scanner input) {
+		System.out.println("===assignment will be added===");
+		System.out.print("assignment name: ");
+		String name = input.next();
+		this.setName(name);
+		
+		char answer = 'x';
+		while(true) {
+			System.out.println("Does this class have professor?(Y/N)");
+			answer = input.next().charAt(0);
+			if(answer == 'y'||answer == 'Y') {		
+				System.out.print("class professor: ");
+				String professor = input.next();
+				this.setProfessor(professor);
+				break;
+			}
+			else if(answer == 'y'||answer == 'Y') {
+				this.setProfessor("");
+				break;
+			}
+			else {
+				
+			}
+		
+		}	
+		System.out.print("class name: ");
+		String classname = input.next();
+		this.setClassname(classname);
+		
+		System.out.print("assignment deadline(yyyy/mm/dd/hh/mm): ");
+		String deadline = input.next();
+		this.setDeadline(deadline);
+	}
+}
